63b18a905b52c7be53914574552680d3c8012f51
lgehr <lukas.gehring@mailbox.org>
2020-04-06 17:00:54 +0200

    Writes package-info Documenation

---
+++ connect-client/src/main/java/berlin/hu/cep/connector/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Provides the classes necessary to configure
+ * a running <strong>Kafka Connect</strong> instance.
+ * <p>
+ * It can configure a <em>Zeebe Source Connector</em> and a <em>Zeebe Sink Connector</em> so
+ * that Kafka can send events between a <strong>zeebe</strong>
+ * and a <strong>siddhi</strong> instance.
+ * </p><p>
+ * It can also configure a <em>MongoDB Connector</em> so that events get logged in a MongoDB instace</p>
+ *
+ * @since 0.0
+ * */
+package berlin.hu.cep.connector;
