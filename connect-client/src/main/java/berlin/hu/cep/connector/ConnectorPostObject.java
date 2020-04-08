05634c77a4fa00ea871c2fa78f8d9a9a27b18e0e
lgehr <lukas.gehring@mailbox.org>
2020-04-08 13:55:15 +0200

    Dokuments ConnectorPostObject

--- connect-client/src/main/java/berlin/hu/cep/connector/ConnectorPostObject.java
+++ connect-client/src/main/java/berlin/hu/cep/connector/ConnectorPostObject.java
@@ -2,12 +2,23 @@ package berlin.hu.cep.connector;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+/**
+ * A class which can be send to <strong>Kafka Connect</strong> as a json-object trought a POST methode.
+ *
+ * @see <a href="https://docs.confluent.io/current/connect/references/restapi.html#post--connectors">The POST methode of the Kafka Connect REST Interface</a>
+ * */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ConnectorPostObject
 {
     private String name;
     private ConnectorConfig config;
 
+    /**
+     * The constructor of {@link ConnectorPostObject}
+     *
+     * @param name Name of the connector to create
+     * @param config Object which holds all configuration parameters for the connector.
+     * */
     public ConnectorPostObject(String name, ConnectorConfig config){
         this.name = name;
         this.config = config;
