ed97c37500dcc3b811171a7177523aba776c07d3
lgehr <lukas.gehring@mailbox.org>
2020-04-09 16:33:45 +0200

    Docuemnts ConnectorConfig and makes is abstract

--- connect-client/src/main/java/berlin/hu/cep/connector/ConnectorConfig.java
+++ connect-client/src/main/java/berlin/hu/cep/connector/ConnectorConfig.java
@@ -1,6 +1,12 @@
 package berlin.hu.cep.connector;
 
-public class ConnectorConfig
+/**
+ * An abstract class for <strong>Kafka Connect</strong> configurations.
+ * <p>As a attribute of an @{@link berlin.hu.cep.connector.ConnectorPostObject ConnectorPostObject} it can be send to a <strong>Kafka Connect</strong>
+ * via a POST-methode of the REST-API.</p>
+ * */
+
+public abstract class ConnectorConfig
 {
 
 }
