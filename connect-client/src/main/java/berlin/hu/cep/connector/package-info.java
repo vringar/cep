3bf2502ef222e308835e772c940db1e2d5d5ba0b
lgehr <lukas.gehring@mailbox.org>
2020-04-10 19:49:52 +0200

    Specifics package documentation

--- connect-client/src/main/java/berlin/hu/cep/connector/package-info.java
+++ connect-client/src/main/java/berlin/hu/cep/connector/package-info.java
@@ -2,14 +2,21 @@
  * Provides the classes necessary to configure
  * a running <strong>Kafka Connect</strong> instance.
  * <p>
- * It can configure a <em>Zeebe Source Connector</em> and a <em>Zeebe Sink Connector</em> so
- * that Kafka can send events between a <strong>zeebe</strong>
+ * The package is build to read custom json configuration files and deploy them accordingly.
+ * To handle json file the package relies on <a href="https://github.com/FasterXML/jackson">FasterXML's Jackson Project</a>
+ * To configure <strong>Kafka Connect</strong> via it's RUST-API this package uses <a href="https://square.github.io/retrofit/">Retrofit</a>.
+ * </p><p>
+ * The package should configure <em>Zeebe Source Connectors</em> and <em>Zeebe Sink Connectors</em> so
+ * that <strong>Kafka</strong> can send events between a <strong>zeebe</strong>
  * and a <strong>siddhi</strong> instance.
  * </p><p>
- * It can also configure a <em>MongoDB Connector</em> so that events get logged in a MongoDB instace</p>
+ * Optionally the package can configure <em>MongoDB Connectors</em> so that events get logged in a MongoDB instance</p>
  *
  * @author Lukas Gehring
  * @author Jost Hermann Triller
  * @since 0.0
+ * @see <a href="kafka.apache.org">Apache Kafka</a>
+ * @see <a href="zeebe.io">Zeebe</a>
+ * @see <a href="siddhi.io">Siddhi</a>
  * */
 package berlin.hu.cep.connector;
