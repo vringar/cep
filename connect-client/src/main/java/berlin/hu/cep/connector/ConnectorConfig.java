96ea12768fe7f2bea9767c39245b5bd989d2d5d1
lgehr <lukas.gehring@mailbox.org>
2020-04-10 19:35:58 +0200

    Adds word

--- connect-client/src/main/java/berlin/hu/cep/connector/ConnectorConfig.java
+++ connect-client/src/main/java/berlin/hu/cep/connector/ConnectorConfig.java
@@ -2,7 +2,7 @@ package berlin.hu.cep.connector;
 
 /**
  * An abstract class for <strong>Kafka Connect</strong> configurations.
- * <p>As a attribute of an @{@link berlin.hu.cep.connector.ConnectorPostObject ConnectorPostObject} it can be send to a <strong>Kafka Connect</strong>
+ * <p>As a attribute of an @{@link berlin.hu.cep.connector.ConnectorPostObject ConnectorPostObject} it can be send to a <strong>Kafka Connect</strong>instance
  * via a POST-methode of the <strong>Kafka Connect</strong>-REST-API.</p>
  * */
 public abstract class ConnectorConfig
