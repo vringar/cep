e8e03bac8b1c70e0b48eb469498e9165b152e2d5
lgehr <lukas.gehring@mailbox.org>
2020-04-27 11:34:28 +0200

    Bums to version 1.0

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn clean package
-java -jar deployer/target/deployer-0.0-SNAPSHOT-jar-with-dependencies.jar -deploy deployer/deployer_config.json
+java -jar deployer/target/deployer-1.0-jar-with-dependencies.jar -deploy deployer/deployer_config.json
