4f596db8036af712cd7f28f34ccbfbec9d4965e0
lgehr <lukas.gehring@mailbox.org>
2020-04-27 16:10:06 +0200

    Rewrites RestAPI Interface documentation

--- connect-client/src/main/java/berlin/hu/cep/connector/RestAPI.java
+++ connect-client/src/main/java/berlin/hu/cep/connector/RestAPI.java
@@ -4,7 +4,10 @@ import retrofit2.Call;
 import retrofit2.http.*;
 
 /**
- * An interace to be used by Retrofit to provide an HTTP interface
+ * An java interface which represents parts of the Kafka Connect Rest Interface.
+ *
+ * The Interface is implemented to be used by the Retrofit HTTP-Client.
+ * Currently only a small part of the Kafka Connect interface is implemented.
  *
  * @author Lukas Gehring
  * @author Leon Haussknecht
@@ -12,16 +15,19 @@ import retrofit2.http.*;
  * @author Jost Hermann Triller
  * @author Stefan Zabka
  *
- * @see <a href="https://square.github.io/retrofit/">The Retrofit dokumentaition</a>
+ * @see <a href="https://square.github.io/retrofit/">The Retrofit documentaition</a>
+ * @see <a href="https://docs.confluent.io/current/connect/references/restapi.html">Kafka Connect Rest Interface</a>
  *
  */
 public interface RestAPI {
 
     /**
-     * Generate Call instance that allows executing an HTTP POST message with json body to kafka connect
      *
-     * @param body is the string that will be send as http-body
-     * @return returns a Retrofit Call instance, that when executed sends the http POST message
+     * Generates a new connector in Kafka Connect.
+     * Accepts a Json as a String.
+     *
+     * @param body A Json formated string which is formated like a {@link ConnectorPostObject}. Should be converted via Jacksons {@code writeValueAsString()} method from a {@code ObjectMapper} instance.
+     * @return A Retrofit Call instance, that when executed sends the http POST message
      * */
     @Headers({
             "Content-Type: application/json",
@@ -31,10 +37,10 @@ public interface RestAPI {
     Call<String> postJson(@Body String body);
 
     /**
-     * Generate Call instance that allows executing an HTTP DELETE message to kafka connect
+     * Deletes a connector from Kafka Connect.
      *
-     * @param name is the name of the Kafka Connect config that needs to be deleted
-     * @return returns a Retrofit Call instance, that when executed sends the http DELETE message
+     * @param name The name of the connector to be deleted
+     * @return A Retrofit Call instance, that when executed sends the http DELETE message
      * */
     @DELETE("connectors/{name}")
     Call<String> delete(@Path("name") String name);
