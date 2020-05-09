c3dbb4427d13044beedac15d8b7f3d87560ea8e2
Stefan Zabka <zabkaste@informatik.hu-berlin.de>
2020-05-09 17:17:01 +0200

    First draft of new pingpong

--- sampleFiles/pingpong/src/main/java/de/huberlin/cep/Connector.java
+++ sampleFiles/pingpong/src/main/java/de/huberlin/cep/Connector.java
@@ -1,23 +1,31 @@
 package de.huberlin.cep;
+
 import io.zeebe.client.ZeebeClient;
 import io.zeebe.client.api.response.DeploymentEvent;
 import io.zeebe.client.api.response.WorkflowInstanceEvent;
+import io.zeebe.client.api.worker.JobWorker;
+
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Connector {
 
-  private ZeebeClient client;
+  private final ZeebeClient client;
+  private final Logger logger;
 
   public ZeebeClient getClient() {
     return client;
   }
 
   public Connector(){
-    this.client = ZeebeClient.newClientBuilder().brokerContactPoint("localhost:26500").build();
+    this("localhost:26500");
   }
 
   public Connector(String address){
     this.client = ZeebeClient.newClientBuilder().brokerContactPoint(address).build();
+    this.logger = LoggerFactory.getLogger(this.getClass());
   }
 
   public void deployWorkflow(String id){
@@ -38,5 +46,23 @@ public class Connector {
       .join();
 
     final long workflowInstanceKey = wfInstance.getWorkflowInstanceKey();
+  }
+
+  public JobWorker deployLoggingWorker(){
+    return client.newWorker()
+      .jobType("message-logger")
+      .handler((jobClient, activatedJob) -> {
+        
+        final Map<String,Object> variables = activatedJob.getVariablesAsMap();
+
+        logger.info("Got response from Siddhi for key {}. This was the {} time a message was sent", variables.get("id"), variables.get("count"));
+        
+        
+        jobClient.newCompleteCommand(activatedJob.getKey())
+          .variables(variables)
+          .send()
+          .join();
+      })
+      .open();
   }
 }
--- sampleFiles/pingpong/src/main/java/de/huberlin/cep/MainClass.java
+++ sampleFiles/pingpong/src/main/java/de/huberlin/cep/MainClass.java
@@ -2,20 +2,36 @@ package de.huberlin.cep;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
+import io.zeebe.client.api.worker.JobWorker;
 
 public class MainClass {
-  public static void main(String[] args){
+
+  public static void main(String[] args) {
     Connector connector = new Connector();
     System.out.println("Connected : \n" + connector.getClient().getConfiguration());
-    if(args.length == 0) {
-      System.out.println("WTF");
-      connector.deployWorkflow("process.bpmn");
-      return;
-    } 
-    Map<String, Object> params = new HashMap<>();
-    params.put("name", "pong");
-    params.put("foo", "bar");
-    params.put("key", Integer.parseInt(args[0]));
-    connector.createWorkflowInstance("ping-pong", params);
+    connector.deployWorkflow("process.bpmn");
+    JobWorker w = connector.deployLoggingWorker();
+    System.out.println("Write \"quit\" to quit or a number to generate a new message");
+    try (Scanner s = new Scanner(System.in)) {
+      boolean run = true;
+      while (run) {
+        switch (s.next().trim()) {
+          case "quit":
+            run = false;
+            break;
+          default:
+            Map<String, Object> params = new HashMap<>();
+            params.put("key", Integer.parseInt(args[0]));
+            connector.createWorkflowInstance("ping-pong", params);
+            break;
+        }
+      }
+    } catch (Exception e) {
+      throw e;
+    }
+    w.close();
+
   }
 }
